a1c4726d11a14ad3b320efdb83feabeda8062634
Jorge <jorge.sv.primero@gmail.com>
2024-03-25 17:03:29 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import mqtt from 'mqtt';
+import {PrismaClient} from '@prisma/client';
 
 const ip = "localhost";
 const client = mqtt.connect(`mqtt://${ip}:1883`)
+
+const prisma = new PrismaClient();
 
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
@@ -10,11 +13,26 @@ client.on('connect', () => {
     });
 });
 
-client.on('message', (topic, message) => {
+client.on('message', async (topic, message) => {
     if (topic === 'authorization') {
+        const user = await prisma.user.findUnique({
+            where: {
+                rfid: message.toString()
+            }
+        })
+        if (user) client.publish('serve', user.temperature.toString());
+
         console.log(`Received message on topic ${topic}: ${message.toString()}`);
     }
     if (topic === 'setTemp'){
+        prisma.user.update({
+            where: {
+                rfid: message.toString()
+            },
+            data: {
+                temperature: parseInt(message.toString())
+            }
+        })
         console.log(`Received message on topic ${topic}: ${message.toString()}`);
     }
 });
